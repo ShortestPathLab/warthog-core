954e4e295a340a3011a1401c9b51fa12af9887bc
Clang-Format Robot <Ryan.Hechenberger@proton.me>
2024-11-12 03:37:59 +0000

    Committing clang-format changes

--- include/warthog/constants.h
+++ include/warthog/constants.h
@@ -30,9 +30,11 @@ struct identity_base
 	IdType id;
 	identity_base() noexcept = default;
 	constexpr explicit identity_base(IdType id_) noexcept : id(id_) { }
-	template <typename IdType2>
-		requires (!std::same_as<IdType, IdType2>)
-	explicit (std::numeric_limits<IdType>::max() < std::numeric_limits<IdType2>::max()) constexpr identity_base(identity_base<Tag, IdType2> alt) : id(IdType{alt.id})
+	template<typename IdType2>
+	requires(!std::same_as<IdType, IdType2>) explicit(
+	    std::numeric_limits<IdType>::max() < std::numeric_limits<IdType2>::
+	        max()) constexpr identity_base(identity_base<Tag, IdType2> alt)
+	    : id(IdType{alt.id})
 	{
 		assert(id == alt.id);
 	}
@@ -71,10 +73,7 @@ struct identity_base
 	{
 		return max();
 	}
-	bool constexpr is_none() const noexcept
-	{
-		return (*this) == none();
-	}
+	bool constexpr is_none() const noexcept { return (*this) == none(); }
 };
 template<class T>
 constexpr bool is_identity_v = std::false_type{};
@@ -108,14 +107,18 @@ constexpr double DBL_TWO = 2.0;
 constexpr double DBL_ROOT_TWO
     = 1.414213562373095048801688724209698078569671875;
 constexpr double DBL_ONE_OVER_TWO = 0.5;
-constexpr double DBL_ONE_OVER_ROOT_TWO = 0.70710678118654752440084436210484903928483593768847403658833986;
-constexpr double DBL_ROOT_TWO_OVER_FOUR = 0.35355339059327376220042218105242451964241796884423701829416993;
+constexpr double DBL_ONE_OVER_ROOT_TWO
+    = 0.70710678118654752440084436210484903928483593768847403658833986;
+constexpr double DBL_ROOT_TWO_OVER_FOUR
+    = 0.35355339059327376220042218105242451964241796884423701829416993;
 constexpr int32_t ONE = 100000;
 
 constexpr uint32_t INF32
-    = std::numeric_limits<uint32_t>::max(); // indicates uninitialised or undefined values
+    = std::numeric_limits<uint32_t>::max(); // indicates uninitialised or
+                                            // undefined values
 constexpr uint64_t INFTY
-    = std::numeric_limits<uint64_t>::max(); // indicates uninitialised or undefined values
+    = std::numeric_limits<uint64_t>::max(); // indicates uninitialised or
+                                            // undefined values
 
 using cost_t = double;
 constexpr cost_t COST_MAX = DBL_MAX;
