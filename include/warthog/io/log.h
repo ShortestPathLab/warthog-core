47bf302de38763329e9a43bb6337e15161074950
Ryan Hechenberger <Ryan.Hechenberger@proton.me>
2025-10-03 11:34:18 +1000

    added logger comments

--- include/warthog/io/log.h
+++ include/warthog/io/log.h
@@ -3,7 +3,36 @@
 
 // io/log.h
 //
-// Logger class.
+// Logging utility.
+//
+// The log_sink is a non-owning copyable struct that points to the data and logging function calls.
+// All logging is performed through log_sink.
+// All classes here are thread safe, follow comments for outlaying cases.
+//
+// Special classes inherit log_sink to provide default functionality.
+// log_sink_std should be used to write to std::cout and std::cerr.
+// log_sink_stream pipe to a stream or open a file stream.
+//
+// The logs are made to be logged to a certain log_level.
+// The WARTHOG_TRACE and others in this header provide interfaces to log to a special
+// logger<> class.  This logger<> class knows at compile time the minimum level to log,
+// and with the use of a macro will be compiled out if that level was not set at compile time
+// when used with the logging macros.
+//
+// The global logger can be acquired with the glog() (logger<>) or glogs() (log_sink), and set with
+// set_glog(log_sink).  The log level of the global logger is set though the WARTHOG_LOG definition,
+// with a default of 1(debug) for debug builds, and 2(information) for release (NDEBUG defined).
+// Macros like WARTHOG_GTRACE will automatically write to the global logger.
+//
+// The _IF will only log if a runtime if true.
+// The _FMT uses the std::format from C++20 to format the output messages.
+// The logging utilities uses dynamic memory std::strings to produce the final log message strings,
+// thus is better to be disabled for release builds through the log level.
+//
+// Log messages produce a messaged as "[TIME LEVEL] msg".
+// The time is formatted in ISO with space (yyyy-mm-dd hh:mm:ss), but can be overridden with
+// define cstring WARTHOG_LOG_TIME.
+// Clock is in local system time, but can be set to UTC by definiong WARTHOG_LOG_CLOCK_UTC.
 //
 // @author: Ryan Hechenberger
 // @created: 2025-09-09
@@ -20,6 +49,7 @@
 #include <utility>
 #include <format>
 
+// default log levels, also for global logger
 #ifdef WARTHOG_LOG
 #define WARTHOG_DEFAULT_LOG_LEVEL WARTHOG_LOG
 #elif !defined(NDEBUG)
@@ -28,8 +58,19 @@
 #define WARTHOG_DEFAULT_LOG_LEVEL 2
 #endif
 
+// specify the log time format
+#ifdef WARTHOG_LOG_TIME
+#define WARTHOG_LOG_TIME_FORMAT WARTHOG_LOG_TIME
+#else
 #define WARTHOG_LOG_TIME_FORMAT "%F %T"
+#endif
+
+// set to use utc time instead of local
+#ifdef WARTHOG_LOG_CLOCK_UTC
+#define WARTHOG_LOG_NOW (std::chrono::utc_clock::now())
+#else
 #define WARTHOG_LOG_NOW (std::chrono::current_zone()->to_local(std::chrono::system_clock::now()))
+#endif
 
 namespace warthog::io
 {
@@ -45,6 +86,11 @@ enum class log_level
 	NONE = 6
 };
 
+/// @brief the log class used to write out to a log.
+///
+/// Takes a pointer for data (must not be null to write).
+/// The array call is the logger for each level.
+/// log will write if able (data != null && call[level] != null).
 struct log_sink
 {
 	using call_type = void (void*, std::string_view msg);
@@ -66,6 +112,7 @@ struct log_sink
 	constexpr const log_sink& sink() const noexcept { return *this; }
 };
 
+/// @brief concept that check if class is shaped with a log_sink.
 template <typename Sink>
 concept LogSink = requires (Sink S, log_level level, std::string_view msg)
 {
@@ -74,9 +121,10 @@ concept LogSink = requires (Sink S, log_level level, std::string_view msg)
 	{ std::as_const(S).sink() } -> std::convertible_to<log_sink>;
 };
 
-/// sink to cout|cerr
-/// Is thread safe as long as std::ios_base::sync_with_stdio(false) for cout and cerr.
-/// User provided file stream is not thread safe.
+/// @brief sink to cout|cerr
+///
+/// Is thread safe as long as std::ios_base::sync_with_stdio(false) has not been called.
+/// Can copy go log_sink and then be destructed without issue.
 struct log_sink_std : log_sink
 {
 	// cerr = true will sink to cerr, false to cout
@@ -90,6 +138,10 @@ struct log_sink_std : log_sink
 	static void write_critical(void*, std::string_view msg);
 };
 
+/// @brief sink to a passed stream, or open and own a filestream.
+///
+/// This class is thread-safe through use of a mutex.
+/// Must be kept in scope at all times while its log_sink is in use.
 class log_sink_stream : public log_sink
 {
 public:
@@ -109,10 +161,6 @@ public:
 	static void write_error(void*, std::string_view msg);
 	static void write_critical(void*, std::string_view msg);
 
-	using log_sink::log;
-	using log_sink::can_log;
-	using log_sink::sink;
-
 protected:
 	std::ostream* log_stream_ = nullptr;
 	std::unique_ptr<std::ofstream> owned_file_;
@@ -120,6 +168,15 @@ protected:
 };
 static_assert(LogSink<log_sink_stream>, "log_stream_sink must be a log_sink.");
 
+/// @brief the logger class that manipulates a log_sink.
+/// @tparam MinLevel the minimum logging level, derived at compile time.
+///         Defaults to the programs default log level.
+///
+/// A wrapper class for a log_sink that allows to control the log_level.
+/// If just using a log_sink, it will always log at every level.
+/// This class is handled specially by the logging macros to only log if log_level
+/// is a min_level.
+/// When setting a long_sink, will clear all pointers below MinLevel.
 template <log_level MinLevel = static_cast<log_level>(WARTHOG_DEFAULT_LOG_LEVEL)>
 struct logger : log_sink
 {
@@ -140,6 +197,8 @@ struct logger : log_sink
 		return *this;
 	}
 
+	/// @brief Overrides MinLevel to level. Can only raise, not lower it.
+	/// @param level 
 	constexpr void set_min_level(log_level level) noexcept
 	{
 		assert(static_cast<size_t>(level) <= static_cast<size_t>(log_level::NONE));
@@ -182,6 +241,7 @@ concept LoggerLevel = Logger<Log> && requires {
 		lg.log(level, msg); \
 }}
 
+// Write msg (string_view) to log lg.
 #define WARTHOG_TRACE(lg,msg) WARTHOG_LOG_LEVEL_(true,lg,::warthog::io::log_level::TRACE,msg)
 #define WARTHOG_DEBUG(lg,msg) WARTHOG_LOG_LEVEL_(true,lg,::warthog::io::log_level::DEBUG,msg)
 #define WARTHOG_INFO(lg,msg) WARTHOG_LOG_LEVEL_(true,lg,::warthog::io::log_level::INFORMATION,msg)
@@ -189,6 +249,7 @@ concept LoggerLevel = Logger<Log> && requires {
 #define WARTHOG_ERROR(lg,msg) WARTHOG_LOG_LEVEL_(true,lg,::warthog::io::log_level::ERROR,msg)
 #define WARTHOG_CRIT(lg,msg) WARTHOG_LOG_LEVEL_(true,lg,::warthog::io::log_level::CRITICAL,msg)
 
+// Write msg (string_view) to log lg if runtime cond is true.
 #define WARTHOG_TRACE_IF(cond,lg,msg) WARTHOG_LOG_LEVEL_(cond,lg,::warthog::io::log_level::TRACE,msg)
 #define WARTHOG_DEBUG_IF(cond,lg,msg) WARTHOG_LOG_LEVEL_(cond,lg,::warthog::io::log_level::DEBUG,msg)
 #define WARTHOG_INFO_IF(cond,lg,msg) WARTHOG_LOG_LEVEL_(cond,lg,::warthog::io::log_level::INFORMATION,msg)
@@ -207,6 +268,7 @@ concept LoggerLevel = Logger<Log> && requires {
 		lg.log(level, std::format(__VA_ARGS__)); \
 }}
 
+// Write formatted message to log, pass as format,args...
 #define WARTHOG_TRACE_FMT(lg,...) WARTHOG_LOG_LEVEL_FMT_(true,lg,::warthog::io::log_level::TRACE,__VA_ARGS__)
 #define WARTHOG_DEBUG_FMT(lg,...) WARTHOG_LOG_LEVEL_FMT_(true,lg,::warthog::io::log_level::DEBUG,__VA_ARGS__)
 #define WARTHOG_INFO_FMT(lg,...) WARTHOG_LOG_LEVEL_FMT_(true,lg,::warthog::io::log_level::INFORMATION,__VA_ARGS__)
@@ -214,6 +276,7 @@ concept LoggerLevel = Logger<Log> && requires {
 #define WARTHOG_ERROR_FMT(lg,...) WARTHOG_LOG_LEVEL_FMT_(true,lg,::warthog::io::log_level::ERROR,__VA_ARGS__)
 #define WARTHOG_CRIT_FMT(lg,...) WARTHOG_LOG_LEVEL_FMT_(true,lg,::warthog::io::log_level::CRITICAL,__VA_ARGS__)
 
+// Write formatted message to log if cond is true, pass as format,args...
 #define WARTHOG_TRACE_FMT_IF(cond,lg,...) WARTHOG_LOG_LEVEL_FMT_(cond,lg,::warthog::io::log_level::TRACE,__VA_ARGS__)
 #define WARTHOG_DEBUG_FMT_IF(cond,lg,...) WARTHOG_LOG_LEVEL_FMT_(cond,lg,::warthog::io::log_level::DEBUG,__VA_ARGS__)
 #define WARTHOG_INFO_FMT_IF(cond,lg,...) WARTHOG_LOG_LEVEL_FMT_(cond,lg,::warthog::io::log_level::INFORMATION,__VA_ARGS__)
@@ -224,10 +287,15 @@ concept LoggerLevel = Logger<Log> && requires {
 // global logger
 
 using global_logger_type = logger<>;
+/// @brief get the global logger, default is set to std::cerr through log_sink_std
 global_logger_type& glog();
+/// @brief get the global log_sink
 const log_sink& glogs();
+/// @brief sets the global log_sink
 void set_glog(log_sink log);
 
+/// @brief A logger that sets itself to glog()
+/// @tparam MinLevel 
 template <log_level MinLevel = log_level::DEBUG>
 struct global_logger : logger<MinLevel>
 {
@@ -237,14 +305,14 @@ struct global_logger : logger<MinLevel>
 	
 	constexpr global_logger& operator=(const global_logger&) noexcept = default;
 
+	/// @brief Updates the sink to the new glog sink.  Must be called if set_glog was used.
 	void resync_global()
 	{
 		static_cast<logger&>(*this) = glog().sink();
 	}
 };
 
-// defines to the global logger
-
+// the global version of the marcos, omits the passing of a logger.
 #define WARTHOG_GTRACE(msg) WARTHOG_TRACE(::warthog::io::glog(),msg)
 #define WARTHOG_GDEBUG(msg) WARTHOG_DEBUG(::warthog::io::glog(),msg)
 #define WARTHOG_GINFO(msg) WARTHOG_INFO(::warthog::io::glog(),msg)
